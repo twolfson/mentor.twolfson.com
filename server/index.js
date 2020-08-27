d0f14f546e49bdfe44d16f6d09ea66853626f8ae
Todd Wolfson <todd@twolfson.com>
2020-08-26 20:38:57 -0700

    Rewrote the wheel (Parcel CLI) so we can do some common deploys/tests

--- server/index.js
+++ server/index.js
@@ -1,21 +1,46 @@
 // Heavily based on https://parceljs.org/api.html
 // Load in our dependencies
-const Bundler = require('parcel-bundler');
+const Bundler = require('parcel');
 
-//
-// Single entrypoint file location:
-const entryFiles = [__dirname + '/index.pug'];
+// Define our constants
+const ENV_DEVELOPMENT = 'development';
+const ENV_PRODUCTION = 'production';
 
-// Bundler options
+// Fallback our environment
+process.env.NODE_ENV = process.env.NODE_ENV || ENV_DEVELOPMENT;
+
+// Set up Parcel configuration
+const entryFiles = [
+  __dirname + '/index.pug',
+  __dirname + '/robots.txt',
+];
 const options = {
+  port: 5000
 };
 
-(async function() {
-  // Initializes a bundler using the entrypoint location and options provided
-  const bundler = new Bundler(entryFiles, options);
+// Extend options on a per-environment basis
+if (process.env.NODE_ENV === ENV_PRODUCTION) {
+  // DEV: This line seems to tell program to quit when done
+  //   https://github.com/parcel-bundler/parcel/blob/v1.10.3/src/cli.js#L198
+  options.production = true;
+}
 
-  // Run the bundler, this returns the main bundle
-  // Use the events if you're using watch mode as this promise will only trigger once and not for every rebuild
-  // const bundle = await bundler.bundle();
-  await bundler.serve();
-})();
+// Generate and export a bundler for extension
+// https://github.com/parcel-bundler/parcel/blob/v1.10.3/src/cli.js#L213-L228
+const bundler = new Bundler(entryFiles, options);
+module.exports = bundler;
+
+// If this file is being run via the CLI, then run the following
+// https://github.com/parcel-bundler/parcel/blob/v1.10.3/src/cli.js#L213-L228
+function async main() {
+  if (process.env.NODE_ENV === ENV_DEVELOPMENT) {
+    bundler.serve();
+  } else if (process.env.NODE_ENV === ENV_PRODUCTION) {
+    bundler.bundle();
+  } else {
+    throw new Error(`\`index.js\` being run in unexpected environment: ${process.env.NODE_ENV}`);
+  }
+};
+if (require.main === module) {
+  main();
+}
