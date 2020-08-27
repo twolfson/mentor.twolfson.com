6981399bb6265ddbf1fe6d0a7d77fb3d079807f0
Todd Wolfson <todd@twolfson.com>
2020-08-27 04:58:47 -0700

    Fixed up HMR loading for multiple .pug files

--- server/index.js
+++ server/index.js
@@ -12,8 +12,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || ENV_DEVELOPMENT;
 
 // Set up Parcel configuration
 const entryFiles = [
-  __dirname + '/index.pug',
-  __dirname + '/contact-success.pug',
+  // DEV: All HMR files are specified first, https://github.com/parcel-bundler/parcel/issues/2340#issuecomment-593166548
+  //   so all HTML files go first, using a glob
+  __dirname + '/{index,contact-success}.pug',
   __dirname + '/robots.txt',
 ];
 const options = {
