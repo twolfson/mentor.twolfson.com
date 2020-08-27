87fa0b6c2fbf57d2f07ef1ce8c1abf8561b0f119
Todd Wolfson <todd@twolfson.com>
2020-08-26 21:07:39 -0700

    Added cheerio assertion for ids

--- test/analytics.js
+++ test/analytics.js
@@ -1,20 +1,30 @@
 // Load in our dependencies
-const { generateBundler } = require('../server/index.js');
+const cheerio = require('cheerio');
 const { expect } = require('chai');
+const { generateBundler } = require('../server/index.js');
 
 // Define our tests
 describe('All generated HTML pages', function () {
   // DEV: If this becomes common, then memoize results in a common test file
-  before(async function () {
+  before(async function generateHTML () {
     this.timeout(30e3);
     this.bundler = generateBundler();
     this.bundle = await this.bundler.bundle();
     // DEV: We could `.map` the `childBundles` set but this is easier
-    this.htmlFile = Array.from(this.bundle.childBundles)[0].entryAsset.generated.html;
-    expect(this.htmlFile).to.be.a('string');
+    this.htmlStr = Array.from(this.bundle.childBundles)[0].entryAsset.generated.html;
+    expect(this.htmlStr).to.be.a('string');
+  });
+  before(function generateCheerio () {
+    this.$ = cheerio.load(this.htmlStr);
   });
 
   it('has an id on each `a` element', function () {
+    let $aEls = this.$('a');
+    expect($aEls.length).to.be.greaterThan(3);
+    $aEls.each((i, aEl) => {
+      let $aEl = this.$(aEl);
+      expect($aEl.attr('id')).to.not.eq(undefined, $aEl + ' id');
+    });
   });
 
   it.skip('all elements have unique ids', function () {
